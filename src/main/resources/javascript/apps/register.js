ee51b32e464c1f2accc08c7fa38c5859b68368b5
Florent BOURASSE <fbourasse@jahia.com>
2020-08-20 14:54:17 +0200

    Modifications for 8.0 compatibility

--- src/main/resources/javascript/apps/register.js
+++ src/main/resources/javascript/apps/register.js
@@ -1,15 +1,6 @@
-import {registry} from '@jahia/ui-extender';
-import {Person} from '@jahia/moonstone/dist/icons';
-import React from 'react';
-import {DefaultEntry} from '@jahia/moonstone/dist/icons';
-
-export const registerRoutes = function () {
-    registry.add('adminRoute', 'downloadHelper', {
-        targets: ['administration-server-systemComponents:10'],
-        requiredPermission: 'administrationAccess',
-        icon: <DefaultEntry/>,
-        label: 'download-helper:settings',
-        isSelectable: true,
-        iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/$lang/settings.downloadHelper.html'
-    });
-};
+window.jahia.uiExtender.registry.add('adminRoute', 'downloadHelper', {
+    targets: ['administration-server-systemHealth:10'],
+    label: 'download-helper:settings',
+    isSelectable: true,
+    iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/$lang/settings.downloadHelper.html'
+});
