e59085c3faa47fad9f59635080ccee3108c3c5f0
Florent BOURASSE <fbourasse@jahia.com>
2020-08-20 16:41:05 +0200

    Modification in order to take into account the translations.

--- src/main/resources/javascript/apps/register.js
+++ src/main/resources/javascript/apps/register.js
@@ -1,6 +1,8 @@
+window.jahia.i18n.loadNamespaces('download-helper');
+
 window.jahia.uiExtender.registry.add('adminRoute', 'downloadHelper', {
     targets: ['administration-server-systemHealth:10'],
-    label: 'download-helper:settings',
+    label: 'download-helper:label',
     isSelectable: true,
     iframeUrl: window.contextJsParameters.contextPath + '/cms/adminframe/default/$lang/settings.downloadHelper.html'
 });
